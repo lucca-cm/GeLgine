2dc60c73806b47f02f1c6d1e0ac462025673d0d9
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-06-28 21:54:46 -0400

    Moved window code into windowManager class

---
+++ src/windowManager.h
@@ -0,0 +1,11 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+class WindowManager {
+    private:
+        GLFWwindow* window;
+    public:
+        WindowManager();
+        ~WindowManager();
+        
+};
