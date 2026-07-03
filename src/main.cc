602326d91c1c3a6a4851191b579b345f9c310e3f
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-03 16:59:04 -0400

    added loop to main

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,15 @@
 #include "windowManager.h"
 
 int main() {
+    WindowManager wm;
+    
+    while (!wm.shouldExit()) {
+        wm.pollEvents();
+        
+        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        wm.swapBuffers();
+    }
     return 0;
 }
