210dedec58824b5498759cce02bee50f4ca2d45d
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-06 23:03:57 -0400

    created a triangle (will split code later)

--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,56 @@
 #include "windowManager.h"
+#include "Shader.h"
+
+GLfloat vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f
+};
+
+const char *vertexSource = R"(#version 330 core
+
+layout (location = 0) in vec3 position;
+
+void main() {
+    gl_Position = vec4(position.x, position.y, position.z, 1.0);
+})";
+
+const char *fragmentSource = R"(#version 330 core
+
+out vec4 color;
+
+void main() {
+    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+})";
 
 int main() {
     WindowManager wm;
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     
+    Shader basicShader(vertexSource, fragmentSource);
+
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
+        GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+        glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
     while (!wm.shouldExit()) {
         wm.pollEvents();
         
         glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(basicShader.getProgram());
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
         wm.swapBuffers();
     }
     return 0;
