e3a1b2c02160132477063a748ddd2df3fd729d92
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-06 22:59:28 -0400

    Added Shader class

---
+++ src/Shader.h
@@ -0,0 +1,30 @@
+#include <glad/glad.h>
+
+class Shader {
+    private:
+        GLuint program;
+
+    public:
+        Shader(const char *vsSource, const char *fsSource) {
+            GLuint vertex, fragment;
+            vertex = glCreateShader(GL_VERTEX_SHADER);
+            glShaderSource(vertex, 1, &vsSource, NULL);
+            glCompileShader(vertex);
+
+            fragment = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(fragment, 1, &fsSource, NULL);
+            glCompileShader(fragment);
+
+            program = glCreateProgram();
+            glAttachShader(program, vertex);
+            glAttachShader(program, fragment);
+            glLinkProgram(program);
+            
+            glDeleteShader(vertex);
+            glDeleteShader(fragment);
+        }
+
+        GLuint& getProgram() {
+            return program;
+        }
+};
