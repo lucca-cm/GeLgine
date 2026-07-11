c411507adcf051b234e13cf5fa0669670707b0ca
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-11 15:46:28 -0400

    Added toggleWireframe

--- src/renderer.h
+++ src/renderer.h
@@ -2,7 +2,16 @@
 
 class Renderer {
     private:
-
+        bool isWireframeEnabled = false;
     public:
-
+        void toggleWireFrame() {
+            if (!isWireframeEnabled) {
+                glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+                isWireframeEnabled = true;
+            }
+            else {
+                glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
+                isWireframeEnabled = false;
+            }
+        }
 };
