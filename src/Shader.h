1adc2563af737706cffb63454682c6ac39abd513
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-12 23:01:59 -0400

    moved Shader code to shader.cc

--- src/Shader.h
+++ src/Shader.h
@@ -3,6 +3,7 @@
 #include <string>
 
 #include <glad/glad.h>
+#include <glm/glm.hpp>
 
 class Shader {
     private:
@@ -15,38 +16,12 @@ class Shader {
             return buff.str();
         }
     public:
-        Shader(const std::string& vertexFile, const std::string& fragFile) {
-            GLuint vertex, fragment;
+        Shader(const std::string& vertexFile, const std::string& fragFile);
+        ~Shader();
 
-            std::string vertexShader = readFile(vertexFile);
-            std::string fragShader = readFile(fragFile);
-            
-            const char *vsSource = vertexShader.c_str();
-            const char *fsSource = fragShader.c_str();
+        bool setUniform(std::string& uniformName, glm::vec4& vector);
 
-            vertex = glCreateShader(GL_VERTEX_SHADER);
-            glShaderSource(vertex, 1, &vsSource, NULL);
-            glCompileShader(vertex);
-
-            fragment = glCreateShader(GL_FRAGMENT_SHADER);
-            glShaderSource(fragment, 1, &fsSource, NULL);
-            glCompileShader(fragment);
-
-            program = glCreateProgram();
-            glAttachShader(program, vertex);
-            glAttachShader(program, fragment);
-            glLinkProgram(program);
-            
-            glDeleteShader(vertex);
-            glDeleteShader(fragment);
-        }
-        ~Shader() {
-            glDeleteProgram(program);
-        }
-        void use() {
-            glUseProgram(program);
-        }
-
+        void use(); 
         GLuint& getProgram() {
             return program;
         }
