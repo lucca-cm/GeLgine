27e37777f2f310d62e21b534d9778724d139cb0c
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-12 23:01:59 -0400

    moved Shader code to shader.cc

---
+++ src/Shader.cc
@@ -0,0 +1,44 @@
+#include "Shader.h"
+
+Shader::Shader(const std::string &vertexFile, const std::string &fragFile) {
+    GLuint vertex, fragment;
+
+    std::string vertexShader = readFile(vertexFile);
+    std::string fragShader = readFile(fragFile);
+    
+    const char *vsSource = vertexShader.c_str();
+    const char *fsSource = fragShader.c_str();
+
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vsSource, NULL);
+    glCompileShader(vertex);
+
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fsSource, NULL);
+    glCompileShader(fragment);
+
+    program = glCreateProgram();
+    glAttachShader(program, vertex);
+    glAttachShader(program, fragment);
+    glLinkProgram(program);
+    
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+}
+
+Shader::~Shader() {
+    glDeleteProgram(program);
+}
+
+bool Shader::setUniform(std::string& uniformName, glm::vec4& vector) {
+    GLint location = glGetUniformLocation(program, uniformName.c_str());
+    if (location == -1)
+        return false;
+    use();
+    glUniform4f(location, vector.x, vector.y, vector.z, vector.w);
+    return true;
+}
+
+void Shader::use() {
+    glUseProgram(program);
+}
