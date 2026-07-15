46af9bbd059be786155cdb9c3d5fa9d567850907
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-14 22:15:16 -0400

    added clear to renderer

--- src/renderer.h
+++ src/renderer.h
@@ -1,9 +1,14 @@
 #include <glad/glad.h>
+#include <glm/glm.hpp>
 
 class Renderer {
     private:
         bool isWireframeEnabled = false;
     public:
+        void clear(glm::vec4 color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f)) {
+            glClearColor(color.r, color.g, color.b, color.a);
+            glClear(GL_COLOR_BUFFER_BIT);
+        }
         void toggleWireFrame() {
             if (!isWireframeEnabled) {
                 glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
