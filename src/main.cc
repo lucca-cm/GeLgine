240404757d6b23f6f0d76124066ac4caa0ecac3b
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-14 22:15:27 -0400

    cleaned up main.cc by using classes created

--- src/main.cc
+++ src/main.cc
@@ -1,40 +1,34 @@
 #include "windowManager.h"
+#include "renderer.h"
 #include "Shader.h"
+#include "Mesh.h"
 
-GLfloat vertices[] = {
+std::vector<GLfloat> vertices = {
+    0.5f, 0.5f, 0.0f, 
+    0.5f, -0.5f, 0.0f, 
     -0.5f, -0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f,
-    0.0f, 0.5f, 0.0f
+    -0.5f, 0.5f, 0.0f 
+};
+
+std::vector<GLuint> indices = {
+    0, 1, 3,
+    1, 2, 3,
 };
 
 int main() {
-    WindowManager wm;
-    GLuint VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    
+    WindowManager wm(800, 600);
+    Renderer renderer;
     Shader basicShader("./shaders/basic.vert", "./shaders/basic.frag");
+    Mesh rectangle(vertices, indices);
 
-    GLuint VAO;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
-        GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
-        glEnableVertexAttribArray(0);
-    glBindVertexArray(0);
+    renderer.toggleWireFrame();
     while (!wm.shouldExit()) {
         wm.pollEvents();
         
-        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        renderer.clear();
 
         basicShader.use();
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glBindVertexArray(0);
+        rectangle.draw();
         wm.swapBuffers();
     }
     return 0;
