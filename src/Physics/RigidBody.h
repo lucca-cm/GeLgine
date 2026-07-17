5c6edced2e4cb0e202e9824b779c50ff5744419b
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-16 23:03:25 -0400

    Added rotational integration

--- src/Physics/RigidBody.h
+++ src/Physics/RigidBody.h
@@ -9,12 +9,14 @@ class RigidBody {
         glm::vec3 velocity;
         glm::vec3 force;
 
+        const float rotationalDamping;
         glm::quat rotation;
         glm::vec3 angularVeloity;
         glm::vec3 torque;
 
         float inverseMass;
         glm::mat3 inverseInertia;
+        glm::mat3 inverseWorldInertia;
         Collider *collider = nullptr;
         
     public:
@@ -27,6 +29,19 @@ class RigidBody {
 
         void integrateLinear(float dt) {
             velocity += inverseMass * force * dt;
-            position += velocity;
+            position += velocity * dt;
+            force = glm::vec3(0.0f);
+        }
+
+        void integrateAngular(float dt) {
+            angularVeloity += inverseWorldInertia * torque * dt;
+            angularVeloity *= powf(rotationalDamping, dt);
+
+            rotation += glm::quat(0.0f, angularVeloity) * rotation * 0.5f * dt;
+            rotation = glm::normalize(rotation);
+
+            glm::mat3 R = glm::mat3_cast(rotation);
+            inverseWorldInertia = R * inverseInertia * glm::transpose(R);
+            torque = glm::vec3(0.0f);
         }
 };
