22ad0fba1c753b2c13c9fdb250691f4f8f8230c0
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-16 21:15:42 -0400

    Added boiler plate to RigidBody class

--- src/RigidBody.h
+++ src/RigidBody.h
@@ -1,14 +1,32 @@
-#include "glm/glm.hpp"
-#include "glm/gtc/quaternion.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
 
 #include "Collider.h"
 
 class RigidBody {
     private:
         glm::vec3 position;
-        glm::quat rotation;
-        Collider *collider = nullptr;
+        glm::vec3 velocity;
+        glm::vec3 force;
 
-    public:
+        glm::quat rotation;
+        glm::vec3 angularVeloity;
+        glm::vec3 torque;
+
+        float inverseMass;
+        glm::mat3 inverseInertia;
+        Collider *collider = nullptr;
         
+    public:
+        void addForce(glm::vec3 f) {
+            force += f;
+        }
+        void addTorque(glm::vec3 t) {
+            torque += t;
+        }
+
+        void integrateLinear(float dt) {
+            velocity += inverseMass * force * dt;
+            position += velocity;
+        }
 };
