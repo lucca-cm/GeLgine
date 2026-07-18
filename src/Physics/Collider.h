9d8396216a17c8b480f8330e8321df8f769d57f1
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-18 15:34:33 -0400

    Added physics namespace

--- src/Physics/Collider.h
+++ src/Physics/Collider.h
@@ -2,21 +2,23 @@
 
 #include <glm/glm.hpp>
 
-struct CollisionPoint {
-    float penetrationDepth;
-    glm::vec3 point;
-};
+namespace Physics {
+    struct CollisionPoint {
+        float penetrationDepth;
+        glm::vec3 point;
+    };
 
-struct CollisionManifold {
-    Collider *first, *second;
+    struct CollisionManifold {
+        Collider *first, *second;
 
-    glm::vec3 normal;
-    std::vector<CollisionPoint> points;
+        glm::vec3 normal;
+        std::vector<CollisionPoint> points;
 
-};
+    };
 
-class Collider {
-    public:
-        virtual ~Collider() = default;
-        virtual glm::vec3 getFurthestPoint(const glm::vec3 d) const = 0;
-};
+    class Collider {
+        public:
+            virtual ~Collider() = default;
+            virtual glm::vec3 getFurthestPoint(const glm::vec3 d) const = 0;
+    };
+}
