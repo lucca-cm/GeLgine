56e45e6128132b76376a15f587792cdb074d895b
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-17 23:27:17 -0400

    Added physics manager

---
+++ src/Physics/PhysicsManager.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <vector>
+
+#include "RigidBody.h"
+#include "Collider.h"
+#include "CollisionHandler.h"
+
+class PhysicsManager {
+    private:
+        std::vector<RigidBody> bodies;
+        std::vector<std::unique_ptr<Collider>> colliders;
+    public:
+        RigidBody& addBody(const RigidBody& b) {
+            bodies.push_back(b);
+
+            return bodies.back();
+        }
+
+        Collider& addCollider(std::unique_ptr<Collider> c) {
+            colliders.push_back(std::move(c));
+
+            return *colliders.back();
+        }
+        void update(float dt) {
+            for (auto& body : bodies) {
+                body.integrate(dt);
+            }
+        }
+};
