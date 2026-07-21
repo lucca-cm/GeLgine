cf01307c67366eb866a0a7d173755e41825ce4a4
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-21 18:14:34 -0400

    moved collider to the top to remove declaration

--- src/Physics/Collider.h
+++ src/Physics/Collider.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
 
 namespace Physics {
-    class Collider;
-
+    class Collider {
+        public:
+            virtual glm::vec3 getFurthestPoint(const glm::vec3& d, const Transform& transform) const = 0;
+    };
     struct CollisionPoint {
         float penetrationDepth;
         glm::vec3 point;
@@ -21,10 +24,5 @@ namespace Physics {
     struct Transform {
         glm::vec3 posistion;
         glm::quat orientation;
-    };
-
-    class Collider {
-        public:
-            virtual glm::vec3 getFurthestPoint(const glm::vec3& d, const Transform& transform) const = 0;
     };
 }
