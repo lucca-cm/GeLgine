816ddd4ff90f6e155792e8f3308875d1bfc0499a
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-20 20:19:46 -0400

    Changed Demo in main code

--- src/main.cc
+++ src/main.cc
@@ -6,6 +6,8 @@
 #include "./Rendering/Mesh.h"
 #include "./Rendering/Camera.h"
 #include "./Physics/RigidBody.h"
+#include "./Physics/BoxCollider.h"
+#include "./Physics/CollisionHandler.h"
 
 std::vector<GLfloat> vertices = {
     // front face
@@ -48,16 +50,23 @@ int main() {
     Graphics::Shader basicShader("./shaders/basic.vert", "./shaders/basic.frag");
     Graphics::Mesh cube(vertices, indices);
 
+    Physics::BoxCollider boxCollider(glm::vec3(0.5f, 0.5f, 0.5f));
+
     glm::vec3 bodyPos(0.0f);
     glm::quat rotation(1.0f, 0.0f, 0.0f, 0.0f);
     glm::mat3 inertia(1.0f);
-    Physics::RigidBody rb(bodyPos, rotation, 1, inertia, 0);
+    Physics::RigidBody rb(bodyPos, rotation, 1, inertia, 1);
+
+    glm::vec3 bPos(2.0f, 0.0f, 0.0f);
+    Physics::RigidBody rigidb(bPos, rotation, 1, inertia, 1);
+    rigidb.setCollider(&boxCollider);
+    rb.setCollider(&boxCollider);
 
     glm::vec3 cameraPosition(0.0f, 0.0f, 3.0f);
     glm::quat cameraRotation(1.0f, 0.0f, 0.0f, 0.0f);
     Graphics::Camera camera(cameraPosition, cameraRotation, 60.0f, 800.0f / 600.0f, 0.1f, 100.0f);
 
-    glm::vec3 gravity(0.0f, -9.81f, 0.0f);
+    //glm::vec3 gravity(0.0f, -9.81f, 0.0f);
 
     glEnable(GL_DEPTH_TEST);
     
@@ -67,10 +76,28 @@ int main() {
 
         wm.updateTime();
 
-        rb.addForce(gravity);
+        glm::vec3 force(0.0f);
+        
+        if (wm.isKeyDown(GLFW_KEY_RIGHT)) {
+            force.x += 1.0f;
+        }
+        if (wm.isKeyDown(GLFW_KEY_LEFT)) {
+            force.x -= 1.0f;
+        }
+        if (wm.isKeyDown(GLFW_KEY_UP)) {
+            force.z += 1.0f;
+        }
+        if (wm.isKeyDown(GLFW_KEY_DOWN)) {
+            force.z -= 1.0f;
+        }
+
+
+
+        rb.addForce(force);
         rb.integrate(wm.getDeltaTime());
 
-        float time = static_cast<float>(glfwGetTime());
+        bool colliding = Physics::CollisionHandler::GJK({rigidb.getCollider(), rigidb.getTransform()},
+                                                        {rb.getCollider(), rb.getTransform()});
         glm::mat4 model =
             glm::translate(glm::mat4(1.0f), rb.getPosition()) *
             glm::mat4_cast(rb.getRotation());
@@ -82,12 +109,17 @@ int main() {
         basicShader.setUniform("model", model);
         basicShader.setUniform(
             "fragColor",
-            glm::vec4(
-                sin(time) * 0.5f + 0.5f,
-                cos(time) * 0.5f + 0.5f,
-                sin(time + glm::pi<float>()) * 0.5f + 0.5f,
-                1.0f
-            )
+            glm::vec4(1.0f, 0.0f, 0.0f, 1.0f)
+        );
+        cube.draw();
+        
+        model =
+            glm::translate(glm::mat4(1.0f), rigidb.getPosition()) *
+            glm::mat4_cast(rigidb.getRotation());
+        basicShader.setUniform("model", model);
+        basicShader.setUniform(
+            "fragColor",
+            (colliding ? glm::vec4(0.0f, 1.0f, 0.0f, 1.0f) : glm::vec4(0.0f, 0.0f, 1.0f, 1.0f))
         );
         cube.draw();
         wm.swapBuffers();
