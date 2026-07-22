2591019d2b38006557bfb50323098c6046e557ff
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-21 22:01:40 -0400

    added naive collision loop for testing

--- src/Physics/PhysicsManager.h
+++ src/Physics/PhysicsManager.h
@@ -10,23 +10,77 @@ namespace Physics {
     class PhysicsManager {
         private:
             std::vector<RigidBody> bodies;
-            std::vector<std::unique_ptr<Collider>> colliders;
+            std::vector<Collider *> colliders;
+            std::vector<CollisionPoint> collisions;
+            std::vector<float> impulses;
+
+            size_t velocityIterations = 5;
         public:
-            RigidBody& addBody(const RigidBody& b) {
+            size_t addBody(const RigidBody& b) {
                 bodies.push_back(b);
 
-                return bodies.back();
+                return bodies.size() - 1;
             }
 
-            Collider& addCollider(std::unique_ptr<Collider> c) {
-                colliders.push_back(std::move(c));
+            size_t addCollider(Collider *c) {
+                colliders.push_back(c);
 
-                return *colliders.back();
+                return colliders.size() - 1;
             }
-            void update(float dt) {
+            
+            RigidBody& getRigidBody(size_t id) {
+                return bodies[id];
+            }
+
+            void step(float dt) {
+                for (size_t i = 0; i < bodies.size(); ++i) {
+                    for (size_t j = i + 1; j < bodies.size(); ++j) {
+                        auto& a = bodies[i];
+                        auto& b = bodies[j];
+
+                        auto c = CollisionHandler::GJK({a.getCollider(), a.getTransform()},
+                                                        {b.getCollider(), b.getTransform()});
+                        if (c.first) {
+                            auto temp = CollisionHandler::EPA(c.second,
+                                            {a.getCollider(), a.getTransform()},
+                                            {b.getCollider(), b.getTransform()});
+                            temp.first = &a;
+                            temp.second = &b;
+                            collisions.push_back(temp);
+                            impulses.push_back(0);
+                        }
+                    }
+                }
+
+                for (int i = 0; i < velocityIterations; i++) {
+                    for (int j = 0; j < collisions.size(); j++) {
+                        auto& c = collisions[j];
+                        
+                        RigidBody& a = *c.first;
+                        RigidBody& b = *c.second;
+
+                        glm::vec3 rA = c.point - a.getPosition(); 
+                        glm::vec3 rB = c.point - b.getPosition();
+
+                        float J = CollisionHandler::solveImpulse(c, a, b);
+
+                        float oldJ = impulses[j];
+                        impulses[j] = std::max(0.0f, impulses[j] + J);
+                        float deltaJ = impulses[j] - oldJ;
+
+                        a.addVelocity(-(deltaJ * a.getInverseMass() * c.normal));
+                        a.addAngularVelocity(-(a.getInverseWorldInertia() * glm::cross(rA, deltaJ*c.normal)));
+                        b.addVelocity((deltaJ * b.getInverseMass() * c.normal));
+                        b.addAngularVelocity((b.getInverseWorldInertia() * glm::cross(rB, deltaJ*c.normal)));
+                    }
+                }
+
                 for (auto& body : bodies) {
                     body.integrate(dt);
                 }
+                
+                collisions.clear();
+                impulses.clear();
             }
     };
 }
