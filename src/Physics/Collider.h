dd967747812a1f1e5455994866888248761ff124
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-28 16:50:54 -0400

    added AABB to collider

--- src/Physics/Collider.h
+++ src/Physics/Collider.h
@@ -6,11 +6,14 @@
 namespace Physics {
     struct Transform;
     class RigidBody;
+    struct AABB;
 
     class Collider {
         public:
             virtual glm::vec3 support(const glm::vec3& d, const Transform& transform) const = 0;
+            virtual AABB computeAABB(const Transform& transform) const = 0;
     };
+
     struct CollisionPoint {
         RigidBody *first, *second;
         float penetrationDepth;
@@ -21,5 +24,10 @@ namespace Physics {
     struct Transform {
         glm::vec3 posistion;
         glm::quat orientation;
+    };
+
+    struct AABB {
+        glm::vec3 center;
+        glm::vec3 halfExtent;
     };
 }
