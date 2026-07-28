82dd44452926fc20e18f22c62217ec002a27aa27
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-27 23:42:58 -0400

    Implemented Sphere collider

---
+++ src/Physics/SphereCollider.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <glm/gtc/quaternion.hpp>
+
+#include "Collider.h"
+
+namespace Physics {
+    class SphereCollider : public Collider {
+        private:
+            float radius;
+        public:
+            SphereCollider(float radius);
+            glm::vec3 getFurthestPoint(const glm::vec3& d, const Transform& transform) const override;
+    };
+}
