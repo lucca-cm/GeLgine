1054014fe95ed7f45e4bee00e66c270331099491
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-27 23:42:58 -0400

    Implemented Sphere collider

---
+++ src/Physics/SphereCollider.cc
@@ -0,0 +1,11 @@
+#include "SphereCollider.h"
+
+namespace Physics {
+    SphereCollider::SphereCollider(float radius) : radius(radius) {}
+
+    glm::vec3 SphereCollider::getFurthestPoint(const glm::vec3& d, const Transform& transform) const {
+        if (glm::dot(d, d) == 0.0f)
+            return transform.posistion;
+        return transform.posistion + glm::normalize(d) * radius;
+    }
+}
