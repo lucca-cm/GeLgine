d6fecc3251bc00dd743f63d7724fbd94ee90617e
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-07-30 20:02:17 -0400

    moved code to PhysicsWorld.cc

--- src/Physics/PhysicsWorld.h
+++ src/Physics/PhysicsWorld.h
@@ -17,75 +17,13 @@ namespace Physics {
 
             size_t velocityIterations = 5;
         public:
-            size_t addBody(const RigidBody& b) {
-                bodies.push_back(b);
-
-                return bodies.size() - 1;
-            }
-
-            size_t addCollider(Collider *c) {
-                colliders.push_back(c);
-
-                return colliders.size() - 1;
-            }
+            size_t addBody(const RigidBody& b);
+            size_t addCollider(Collider *c);
             
-            RigidBody& getRigidBody(size_t id) {
-                return bodies[id];
-            }
+            RigidBody& getRigidBody(size_t id);
 
-            void cacheCurrentAABB() {
-                for (auto& rb : bodies) {
-                    aabbCache.push_back(rb.getBodyAABB());
-                }
-            }
+            void cacheCurrentAABB();
 
-            void step(float dt) {
-                cacheCurrentAABB();
-                for (size_t i = 0; i < bodies.size(); ++i) {
-                    for (size_t j = i + 1; j < bodies.size(); ++j) {
-                        auto& a = bodies[i];
-                        auto& b = bodies[j];
-                        
-                        if (CollisionHandler::checkBroadPhase(aabbCache[i], aabbCache[j])) {
-                            auto v = CollisionHandler::checkCollision(a, b);
-                            collisions.insert(collisions.end(), v.begin(), v.end());
-                            for (auto&& _ : v) {
-                                impulses.push_back(0.0f);
-                            }
-                        }
-                    }
-                }
-                
-                for (size_t i = 0; i < velocityIterations; i++) {
-                    for (size_t j = 0; j < collisions.size(); j++) {
-                        auto& c = collisions[j];
-                        
-                        RigidBody& a = *c.first;
-                        RigidBody& b = *c.second;
-
-                        glm::vec3 rA = c.point - a.getPosition(); 
-                        glm::vec3 rB = c.point - b.getPosition();
-
-                        float J = CollisionHandler::solveImpulse(c, a, b);
-
-                        float oldJ = impulses[j];
-                        impulses[j] = std::max(0.0f, impulses[j] + J);
-                        float deltaJ = impulses[j] - oldJ;
-
-                        a.addVelocity((deltaJ * a.getInverseMass() * c.normal));
-                        a.addAngularVelocity((a.getInverseWorldInertia() * glm::cross(rA, deltaJ*c.normal)));
-                        b.addVelocity(-(deltaJ * b.getInverseMass() * c.normal));
-                        b.addAngularVelocity(-(b.getInverseWorldInertia() * glm::cross(rB, deltaJ*c.normal)));
-                    }
-                }
-
-                for (auto& body : bodies) {
-                    body.integrate(dt);
-                }
-                
-                collisions.clear();
-                impulses.clear();
-                aabbCache.clear();
-            }
+            void step(float dt);
     };
 }
