6c4cd555be5dd49f686d94b50ae581d5e43480b9
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-08-01 21:35:56 -0400

    Created PlaneCollider

---
+++ src/Physics/PlaneCollider.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <glm/gtc/quaternion.hpp>
+
+#include "Collider.h"
+
+namespace Physics {
+    class PlaneCollider : public Collider {
+        private:
+            glm::vec3 normal;
+            float distance;
+        public:
+            PlaneCollider(glm::vec3 normal, float distance);
+            glm::vec3 support(const glm::vec3& d, const Transform& transform) const override;
+            AABB computeAABB(const Transform& transform) const override;
+    };
+}
