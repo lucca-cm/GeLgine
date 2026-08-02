9b6dc1a8c110632d773b3e0e563824a7bd0befe5
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-08-02 15:22:17 -0400

    added camera controls

--- src/main.cc
+++ src/main.cc
@@ -27,26 +27,34 @@ int main() {
     Physics::BoxCollider boxCollider(glm::vec3(0.5f, 0.5f, 0.5f));
     Physics::SphereCollider sphereCollider(0.5f);
 
-    glm::vec3 bodyPos(0.0f);
+    glm::vec3 bodyPos(0.0f, 1.0f, 0.0f);
     glm::quat rotation(1.0f, 0.0f, 0.0f, 0.0f);
     glm::mat3 inertia(1.0f);
     Physics::RigidBody rb(bodyPos, rotation, 1, inertia, 1);
 
-    glm::vec3 bPos(2.0f, 0.0f, 0.0f);
+    glm::vec3 bPos(2.0f, 1.0f, 0.0f);
     Physics::RigidBody rigidb(bPos, rotation, 1, inertia, 1);
     rigidb.setCollider(&boxCollider);
     rb.setCollider(&sphereCollider);
 
+    glm::mat3 Iner(0.0f);
+    Physics::RigidBody plane(glm::vec3(0.0f), rotation, 0, inertia, 0);
+    Physics::PlaneCollider planeCollider(glm::vec3(0.0f, 1.0f, 0.0f), 0.0f);
+    plane.setCollider(&planeCollider);
+
     auto aId = pm.addBody(rigidb);
     auto bId = pm.addBody(rb);
+    pm.addBody(plane);
+    pm.addCollider(&planeCollider);
     pm.addCollider(&boxCollider);
     pm.addCollider(&sphereCollider);
+
 
     glm::vec3 cameraPosition(0.0f, 0.0f, 3.0f);
     glm::quat cameraRotation(1.0f, 0.0f, 0.0f, 0.0f);
     Graphics::Camera camera(cameraPosition, cameraRotation, 60.0f, 800.0f / 600.0f, 0.1f, 100.0f);
 
-    //glm::vec3 gravity(0.0f, -9.81f, 0.0f);
+    glm::vec3 gravity(0.0f, -5.0f, 0.0f);
 
     glEnable(GL_DEPTH_TEST);
     
@@ -80,8 +88,27 @@ int main() {
 
 
         a.addForce(force);
+        a.addForce(gravity);
+        b.addForce(gravity);
         pm.step(wm.getDeltaTime());
         
+        glm::vec3 deltaPos(0.0f);
+        if (wm.isKeyDown(GLFW_KEY_D)) {
+            deltaPos.x += 0.1f;
+        }
+        if (wm.isKeyDown(GLFW_KEY_A)) {
+            deltaPos.x -= 0.1f;
+        }
+        if (wm.isKeyDown(GLFW_KEY_W)) {
+            deltaPos.y += 0.1f;
+        }
+        if (wm.isKeyDown(GLFW_KEY_S)) {
+            deltaPos.y -= 0.1f;
+        }
+        
+        camera.moveBy(deltaPos);
+
+        camera.updateCamera();
         bool colliding = Physics::CollisionHandler::GJK({a.getCollider(), a.getTransform()},
                                                         {b.getCollider(), b.getTransform()}).first;
         cube.transformModelMatrix(a.getPosition(), a.getRotation());
