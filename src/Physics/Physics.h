cd972256c9f93986be2a313deaee4f3dbeaf7093
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-08-26 12:11:17 -0400

    Added Physics.h

---
+++ src/Physics/Physics.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "BoxCollider.h"
+#include "Collider.h"
+#include "CollisionHandler.h"
+#include "PhysicsWorld.h"
+#include "PlaneCollider.h"
+#include "RigidBody.h"
+#include "SphereCollider.h"
