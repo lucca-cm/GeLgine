fd5479f4739fb372261686aa78d52bfc463f4708
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-08-26 09:51:24 -0400

    removed redundant Physics::

--- src/Physics/PhysicsWorld.cc
+++ src/Physics/PhysicsWorld.cc
@@ -11,17 +11,17 @@ namespace Physics {
         return bodies.size() - 1;
     }
 
-    size_t Physics::PhysicsWorld::addCollider(Collider *c) {
+    size_t PhysicsWorld::addCollider(Collider *c) {
         colliders.push_back(c);
 
         return colliders.size() - 1;
     }
     
-    RigidBody& Physics::PhysicsWorld::getRigidBody(size_t id) {
+    RigidBody& PhysicsWorld::getRigidBody(size_t id) {
         return bodies[id];
     }
 
-    void Physics::PhysicsWorld::cacheCurrentAABB() {
+    void PhysicsWorld::cacheCurrentAABB() {
         aabbCache.clear();
         aabbCache.reserve(bodies.size());
         for (auto& rb : bodies) {
@@ -29,7 +29,7 @@ namespace Physics {
         }
     }
 
-    void Physics::PhysicsWorld::step(float dt) {
+    void PhysicsWorld::step(float dt) {
         collisions.clear();
         impulses.clear();
         cacheCurrentAABB();
@@ -68,6 +68,9 @@ namespace Physics {
             }
         }
         
+        for (auto& collision : collisions) {
+            CollisionHandler::correctPosition(collision.first, collision.second, collision.normal, collision.penetrationDepth);
+        }
         for (size_t i = 0; i < velocityIterations; i++) {
             for (size_t j = 0; j < collisions.size(); j++) {
                 auto& c = collisions[j];
