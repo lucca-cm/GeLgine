4bab0f8419e10b85a108362c70aadd541a47e3fc
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-08-27 11:19:12 -0400

    Added Engine class

---
+++ src/GeLgine.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Game.h"
+#include "Physics/Physics.h"
+#include "Rendering/Rendering.h"
+#include "WindowManager.h"
+
+namespace GeLgine {
+    class Engine {
+        void run(Game& game);
+    };   
+}
