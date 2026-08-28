69406c264bbfc8c4333cecefee8df9c7b123a636
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-08-28 09:04:00 -0400

    Added context

--- src/Game.h
+++ src/Game.h
@@ -2,9 +2,15 @@
 
 namespace GeLgine {
     class Game {
+        protected:
+            GeLgineContext* ctx;
         public:
             int width = 800, height = 600;
             float fixedDt = 1.0f / 60.0f;
+
+            void setContext(GeLgineContext c) {
+                ctx = &c;
+            }
             virtual void onStart() = 0;
             virtual void onUpdate(float dt) = 0;
             virtual void onFixedUpdate() = 0;
