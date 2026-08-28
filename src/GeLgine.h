ae0c5c2e98846d5036d19ef9f42dc6b32a771a8b
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-08-28 09:04:31 -0400

    Added world, renderer and window to Engine

--- src/GeLgine.h
+++ src/GeLgine.h
@@ -6,7 +6,28 @@
 #include "WindowManager.h"
 
 namespace GeLgine {
+    struct GeLgineContext {
+        Physics::PhysicsWorld& physics;
+        Graphics::Renderer& renderer;
+        WindowManager& window;
+    };
     class Engine {
-        void run(Game& game);
+        private:
+            Physics::PhysicsWorld world;
+            Graphics::Renderer renderer;
+            WindowManager wm;
+            
+        public:
+            Engine() : wm(0, 0) {}
+            GeLgineContext getContext() {
+                auto c = GeLgineContext{
+                    .physics = world,
+                    .renderer = renderer,
+                    .window = wm,
+                };
+
+                return c;
+            }
+            void run(Game& game);
     };   
 }
