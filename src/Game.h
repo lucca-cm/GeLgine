c227c1122e8573e5bd5edff9e57d39614da8f383
Lucca <bedrock_benzene.0e@icloud.com>
2026-09-01 22:16:08 -0400

    added fowards reference for GeLgineContext

--- src/Game.h
+++ src/Game.h
@@ -1,6 +1,7 @@
 #pragma once
 
 namespace GeLgine {
+    struct GeLgineContext;
     class Game {
         protected:
             GeLgineContext* ctx;
@@ -8,8 +9,8 @@ namespace GeLgine {
             int width = 800, height = 600;
             float fixedDt = 1.0f / 60.0f;
 
-            void setContext(GeLgineContext c) {
-                ctx = &c;
+            void setContext(GeLgineContext *c) {
+                ctx = c;
             }
             virtual void onStart() = 0;
             virtual void onUpdate(float dt) = 0;
