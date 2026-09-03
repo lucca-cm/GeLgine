1007dc9f5e56bfbf012a732af7c47d2644e726cb
Lucca <bedrock_benzene.0e@icloud.com>
2026-09-03 12:14:09 -0400

    Added createPlane and getRigidBody to PhysicsContext

--- src/Engine/PhysicsContext.h
+++ src/Engine/PhysicsContext.h
@@ -8,12 +8,24 @@ namespace GeLgine {
             Physics::PhysicsWorld *world;
         public:
             PhysicsContext(Physics::PhysicsWorld *world) : world(world) {}
-            void addBody(const Physics::RigidBody &b) {
-                world->addBody(b);
+            size_t addBody(const Physics::RigidBody &b) {
+                return world->addBody(b);
+            }
+
+            Physics::RigidBody &getRigidBody(const size_t id) {
+                return world->getRigidBody(id);
             }
 
             void addCollider(Physics::Collider *c) {
                 world->addCollider(c);
+            }
+
+            void createPlane(glm::vec3 normal, float distance) {
+                Physics::RigidBody plane(glm::vec3(0.0f), glm::quat(1.0f, 0.0f, 0.0f, 0.0f), 0, glm::mat3(1.0f), 0);
+                auto *planeCollider = new Physics::PlaneCollider(normal, distance);
+                plane.setCollider(planeCollider);
+                addBody(plane);
+                addCollider(planeCollider);
             }
     };
 }
