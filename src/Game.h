980084bf8da00c2a30090ddb45562eda8550241f
Lucca <bedrock_benzene.0e@icloud.com>
2026-09-03 12:12:13 -0400

    fixed width and height  initialization

--- src/Game.h
+++ src/Game.h
@@ -6,8 +6,11 @@ namespace GeLgine {
         protected:
             GeLgineContext* ctx;
         public:
-            int width = 800, height = 600;
-            float fixedDt = 1.0f / 60.0f;
+            const int width = 800, height = 600;
+            const float fixedDt = 1.0f / 60.0f;
+
+            Game(int width = 800, int height = 600, float fixedDt = 1.0f / 60.0f)
+                : width(width), height(height), fixedDt(fixedDt) {}
 
             void setContext(GeLgineContext *c) {
                 ctx = c;
