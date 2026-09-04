68a1716360247b989e7e53ccbb3c5a408fb9cc82
Lucca <bedrock_benzene.0e@icloud.com>
2026-09-04 09:57:54 -0400

    moved GelgineContext to context.h

--- src/Engine/Context.h
+++ src/Engine/Context.h
@@ -4,3 +4,15 @@
 #include "InputContext.h"
 #include "PhysicsContext.h"
 #include "WindowContext.h"
+
+namespace Gelgine {
+    struct GelgineContext {
+        PhysicsContext physics;
+        InputContext input;
+        GraphicsContext graphics;
+        WindowContext window;
+        
+        GelgineContext(Physics::PhysicsWorld *world, Graphics::Renderer *ren, WindowManager *window)
+        : physics(world), input(window), graphics(ren), window(window) {}
+    };
+}
