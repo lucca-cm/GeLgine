124e8c01742f73ba75ce9f33a82357ea87a6bc85
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-09-08 10:04:34 -0400

    Added MeshComponent

---
+++ src/Engine/Components/MeshComponent.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <memory>
+
+#include "Component.h"
+#include "GraphicsComponent.h"
+#include "../../Rendering/Mesh.h"
+
+namespace Gelgine {
+    template<typename T>
+    using Uniform = std::pair<const char *, T>;
+ 
+    class MeshComponent : GraphicsComponent {
+        private:
+            Graphics::Mesh mesh;
+        public:
+            MeshComponent(GameObject *owner, std::vector<GLfloat> vertices, std::vector<GLuint> indices) 
+            : GraphicsComponent(owner), mesh(vertices, indices) {}
+            
+            void draw(Graphics::Renderer &ren) override {
+                ren.draw(mesh);
+            }
+            
+    };
+}
