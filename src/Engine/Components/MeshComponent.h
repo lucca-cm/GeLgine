74f7b2eae966a356909dbcf272b81a114aa4ba66
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-09-10 09:14:30 -0400

    replaced draw with drawObject

--- src/Engine/Components/MeshComponent.h
+++ src/Engine/Components/MeshComponent.h
@@ -13,13 +13,12 @@ namespace Gelgine {
     class MeshComponent : GraphicsComponent {
         private:
             Graphics::Mesh mesh;
+
+            void drawObject(Graphics::Renderer &ren) override {
+                ren.draw(mesh);
+            }
         public:
             MeshComponent(GameObject *owner, std::vector<GLfloat> vertices, std::vector<GLuint> indices) 
             : GraphicsComponent(owner), mesh(vertices, indices) {}
-            
-            void draw(Graphics::Renderer &ren) override {
-                ren.draw(mesh);
-            }
-            
     };
 }
