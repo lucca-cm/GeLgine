e05a080d5afad64a3d325a057194008b22b4dcd9
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-09-10 09:14:17 -0400

    modified graphicsComponent interface

--- src/Engine/Components/GraphicsComponent.h
+++ src/Engine/Components/GraphicsComponent.h
@@ -5,8 +5,23 @@
 
 namespace Gelgine {
     class GraphicsComponent : public Component {
+        private:
+            bool shouldDraw = false;
+
+            virtual void drawObject(Graphics::Renderer &ren) = 0;
         public:
             GraphicsComponent(GameObject *owner) : Component(owner) {}
-            virtual void draw(Graphics::Renderer &ren) = 0;
+            void attach() override {
+                shouldDraw = true;
+            }
+
+            void dettach() override {
+                shouldDraw = false;
+            }
+
+            void draw(Graphics::Renderer &ren) {
+                if (shouldDraw)
+                    drawObject(ren);
+            }
     };
 }
