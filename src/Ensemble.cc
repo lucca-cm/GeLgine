6d9e57e42af3cdfdb35beb0ea52612005fac0bbe
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-09-15 09:35:24 -0400

    fixed issues with Ensemble class + moved createObject to Ensemble.h

--- src/Ensemble.cc
+++ src/Ensemble.cc
@@ -1,14 +1,4 @@
 #include "Ensemble.h"
 
 
-namespace Gelgine {
-    template <typename T, typename... Args>
-    T &Gelgine::Ensemble::createObject(Args &&...args) {
-        auto obj = std::make_unique<T>(this, std::forward<Args>(args)...);
-
-        T& ref = *obj;
-        gameObjects.push_back(std::move(obj));
-
-        return ref;
-    }
-}
+namespace Gelgine {}
