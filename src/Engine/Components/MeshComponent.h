62a5e0af6a4b3ea6a4f1fe923fceb6034ecae4be
Lucca C. M. <bedrock_benzene.0e@icloud.com>
2026-09-15 09:39:38 -0400

    Added material and updateMeshTransform to MeshComponent.h

--- src/Engine/Components/MeshComponent.h
+++ src/Engine/Components/MeshComponent.h
@@ -10,15 +10,20 @@ namespace Gelgine {
     template<typename T>
     using Uniform = std::pair<const char *, T>;
  
-    class MeshComponent : GraphicsComponent {
+    class MeshComponent : public GraphicsComponent {
         private:
             Graphics::Mesh mesh;
+            Graphics::Material material;
 
             void drawObject(Graphics::Renderer &ren) override {
-                ren.draw(mesh);
+                ren.draw(mesh, material, Graphics::Uniform<glm::vec4>{"fragColor", glm::vec4(1.0f, 0.0f, 0.0f, 1.0f)});
             }
         public:
-            MeshComponent(GameObject *owner, std::vector<GLfloat> vertices, std::vector<GLuint> indices) 
-            : GraphicsComponent(owner), mesh(vertices, indices) {}
+            MeshComponent(GameObject *owner, std::vector<GLfloat> vertices, std::vector<GLuint> indices, Graphics::Material material) 
+            : GraphicsComponent(owner), mesh(vertices, indices), material(material) {}
+            
+            void updateMeshTransform(glm::vec3 position, glm::quat rotation) {
+                mesh.transformModelMatrix(position, rotation);
+            }
     };
 }
